c14e9b0e07214388c7e21f7ffb0ccb3c4350b578
Aman Bhargava <bhargavaman@outlook.com>
2019-07-04 02:20:02 +0530

    No styling on this, but it works!

---
+++ talk.js
@@ -0,0 +1,34 @@
+const btn = document.querySelector(".talk");
+const content = document.querySelector(".content");
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.lang = "hi-IN";
+
+recognition.onstart = () => {
+};
+
+recognition.onresult = event => {
+  const currentText = event.resultIndex;
+
+  const transcript = event.results[currentText][0].transcript;
+  content.textContent = transcript;
+  readOutLoud(transcript);
+};
+
+btn.addEventListener("click", () => {
+  recognition.start();
+});
+
+function readOutLoud(message) {
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = message;
+  speech.volume = 1;
+  speech.rate = 1;
+  speech.pitch = 1;
+  speech.lang = "hi-IN";
+
+  window.speechSynthesis.speak(speech);
+}
