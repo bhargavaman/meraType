4eca8bf59cae7387180dcbe53270ce14823556f4
Aman Bhargava <bhargavaman@outlook.com>
2019-07-29 19:09:02 +0530

    added loading thing

--- talk.js
+++ talk.js
@@ -16,11 +16,13 @@ recognition.onresult = event => {
 
   const transcript = event.results[currentText][0].transcript;
   content.textContent = transcript;
+  btn.classList.remove("is-loading");
   readOutLoud(transcript);
 };
 
 btn.addEventListener("click", () => {
   recognition.start();
+  btn.classList.add("is-loading");
 });
 
 function readOutLoud(message) {
